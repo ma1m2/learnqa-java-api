90a974cbcb054ae7bea3654f9af4534550b03ce8
other <other@mail.ru>
2022-09-23 20:59:47 +0300

    Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,29 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
         System.out.println("Hello from Svetlana!");
+    }
+
+    @Test
+    public void testGetHello(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+
+    /**
+     * Ex4: GET-запрос
+     */
+    @Test
+    public void testGetText(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
