68985710896d4ce8ccede8a3da410688d3e387df
other <other@mail.ru>
2022-09-24 15:18:14 +0300

    2l_04m Params Query

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,6 +2,9 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
@@ -10,8 +13,14 @@ public class HelloWorldTest {
 
     @Test
     public void testGetHello(){
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
+
         Response response = RestAssured
+                .given()
+                .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
+                //.get("https://playground.learnqa.ru/api/hello?name=Svetlana")
                 .andReturn();
         response.prettyPrint();
     }
