1e2e5109b129c4919bd4b8be461e1188bdd75c74
other <other@mail.ru>
2022-09-24 17:00:50 +0300

    2l_05m JsonPath

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
@@ -6,6 +7,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HelloWorldTest {
+    Map<String, String> params = new HashMap<>();
+    @Test
+    public void testJsonPathNegative(){
+        params.put("name", "Mark");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer2 = response.get("answer2");
+        if(answer2 == null){
+            System.out.println("The key 'answer2' is absent!");
+        }else {
+            System.out.println(answer2);
+        }
+    }
+    @Test
+    public void testJsonPath(){
+        params.put("name", "Mark");
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+
+        String answer = response.get("answer");
+        System.out.println(answer);
+    }
     @Test
     public void testHelloWorld(){
         System.out.println("Hello from Svetlana!");
@@ -13,9 +43,7 @@ public class HelloWorldTest {
 
     @Test
     public void testGetHello(){
-        Map<String, String> params = new HashMap<>();
         params.put("name", "John");
-
         Response response = RestAssured
                 .given()
                 .queryParams(params)
