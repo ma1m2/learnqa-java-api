cb73d9d739147f9fd2e4182bb538e4bcda695d2c
other <other@mail.ru>
2022-09-30 16:48:26 +0300

    Ex13: User Agent

--- src/test/java/lessons/HomeWorkTest.java
+++ src/test/java/lessons/HomeWorkTest.java
@@ -1,11 +1,14 @@
 package lessons;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
@@ -14,8 +17,36 @@ import java.util.Map;
 
 import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomeWorkTest {
+    /**
+     * Ex13: User Agent
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',Mobile,No,Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',Mobile,Chrome,iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',Googlebot,Unknown,Unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',Web,Chrome,No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',Mobile,No,iPhone"
+    })
+    public void testUserAgent(String userAgent, String platExp, String browExp, String deviceExp){
+        Response response = RestAssured
+                .given()
+                .header("user-agent",userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .then().log().everything().extract().response();
+
+        String platform = response.jsonPath().getString("platform");
+        String browser = response.jsonPath().getString("browser");
+        String device = response.jsonPath().getString("device");
+        assertEquals(platExp, platform, "Unexpected platform " + platform);
+        assertEquals(browExp, browser, "Unexpected platform " + browser);
+        assertEquals(deviceExp, device, "Unexpected platform " + device);
+
+    }
+
     /**
      * Ex12 homework_header
      */
